2d7e04c3ed267bab5e7b323dbdc40df99046d57c
MaeBeau19 <mae.data.analyst@gmail.com>
2024-09-10 21:06:00 -0400

    sqlalchemy commit

---
+++ app.py
@@ -0,0 +1,38 @@
+# Import the dependencies.
+
+import numpy as np
+import sqlalchemy 
+import pandas as pd
+import datetime as dt
+
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import session
+
+
+
+#################################################
+# Database Setup
+#################################################
+
+
+# reflect an existing database into a new model
+
+# reflect the tables
+
+
+# Save references to each table
+
+
+# Create our session (link) from Python to the DB
+
+
+#################################################
+# Flask Setup
+#################################################
+
+
+
+
+#################################################
+# Flask Routes
+#################################################
